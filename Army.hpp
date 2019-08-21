7895b615034901f85d6a4da5ed9262956e8b5477
FirstTitan <petro3333@outlook.com>
2019-08-22 00:41:30 +0300

    [behavior] Just nothing changed

--- Army.hpp
+++ Army.hpp
@@ -1,3 +1,8 @@
 #include "ability/Ability.hpp"
+#include "ability/MagicAbility.hpp"//**********
+#include "ability/DefaultAbility.hpp"//**********
 #include "state/State.hpp"
+#include "state/MagicState.hpp"
+#include "spell/Spell.hpp"//**********
+#include "spellcaster/SpellCaster.hpp"//*********
 #include "unit/Unit.hpp"
