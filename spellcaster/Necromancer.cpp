27d26246d272d1b1ca00b0eba026c48900844661
FirstTitan <petro3333@outlook.com>
2019-08-30 05:10:58 +0300

    [magic] It's undead

--- spellcaster/Necromancer.cpp
+++ spellcaster/Necromancer.cpp
@@ -3,6 +3,7 @@
 Necromancer::Necromancer(const char* title, int hitPoints, int damage, int mana)
     : SpellCaster(title, hitPoints, damage, mana) {
     this->magicAbility = new NecromancerAbility(this, new FireBall(30, 30));
+    this->undead = true;
 }
 
 Necromancer::~Necromancer() {};
